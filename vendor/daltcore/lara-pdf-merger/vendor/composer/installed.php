9bf7452cce9e4f22006c11422c5d4661bec5c2a9
Mauricio Ramos <mauri334455@gmail.com>
2022-03-15 17:19:36 -0600

    Actualizacion 2.2.9

---
+++ vendor/daltcore/lara-pdf-merger/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5862d284867a69f82d6a7b0ee4c39ba1334fb880',
+        'name' => 'daltcore/lara-pdf-merger',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daltcore/lara-pdf-merger' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5862d284867a69f82d6a7b0ee4c39ba1334fb880',
+            'dev_requirement' => false,
+        ),
+    ),
+);
