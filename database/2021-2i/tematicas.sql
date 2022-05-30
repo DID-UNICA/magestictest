5a107c19ab646f276af4a41a87d430d2d34e68cb
Mauricio Ramos <mauri334455@gmail.com>
2022-05-30 09:23:34 -0500

    Archivo de tematicas

---
+++ database/2021-2i/tematicas.sql
@@ -0,0 +1,1 @@
+copy historico_tematicas(id,nombre_curso,nombres_profesor,apellido_paterno_profesor,apellido_materno_profesor,email_profesor,telefono,tematica,semestre_anio,semestre_pi,semestre_si) from 'E:\tematicas\Tematicas.csv' delimiter '|' csv;
