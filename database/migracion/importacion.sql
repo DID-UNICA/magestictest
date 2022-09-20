d6e02cfeaa582e2ed63616184b8bdd88243c4230
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-09-20 15:45:58 -0500

    Actualizacion 2.3.7

--- database/migracion/importacion.sql
+++ database/migracion/importacion.sql
@@ -26,9 +26,9 @@ copy catalogo_cursos(id,clave_curso,coordinacion_id,nombre_curso,tipo,antecedent
 
 copy cursos(id,semestre_anio,semestre_pi,semestre_si,fecha_inicio,fecha_fin,hora_inicio,hora_fin,dias_semana,numero_sesiones,sesiones,acreditacion,costo,cupo_maximo,cupo_minimo,fecha_envio_constancia,fecha_envio_reconocimiento,num_modulo,catalogo_id,salon_id,diplomado_id,sgc) from 'E:\CDD\migracion\Cursos.csv' delimiter '|' csv;
 
-copy participante_curso(id,acreditacion,asistencia,confirmacion,pago_curso,cancelacion,estuvo_en_lista,monto_pago,espera,causa_no_acreditacion,calificacion,contesto_hoja_evaluacion,curso_id,profesor_id) from 'E:\CDD\migracion\Participante_curso.csv' delimiter '|' csv;
+copy participante_curso(id,acreditacion,asistencia,confirmacion,pago_curso,cancelacion,estuvo_en_lista,monto_pago,espera,causa_no_acreditacion,calificacion,contesto_hoja_evaluacion,curso_id,profesor_id,folio_inst) from 'E:\CDD\migracion\Participante_curso.csv' delimiter '|' csv;
 
-copy profesor_curso(id,curso_id,profesor_id,tema_seminario_id,folio_inst,fecha_envio,fecha_exposicion) from 'E:\CDD\migracion\Profesor_curso.csv' delimiter ',' csv;
+copy profesor_curso(id,curso_id,profesor_id,folio_inst,tema_seminario_id,fecha_envio,fecha_exposicion) from 'E:\CDD\migracion\Profesor_curso.csv' delimiter ',' csv;
 
 copy _evaluacion_final_curso(id, p1_1,p1_2,p1_3,p1_4,p1_5,p2_1,p2_2,p2_3,p2_4,p3_1,p3_2,p3_3,p3_4,p7,p8,p9,sug,conocimiento,horarios,horarioi,participante_curso_id) from 'E:\CDD\migracion\Evaluacion_final_curso.csv' delimiter '|' csv;
 
@@ -37,14 +37,14 @@ copy _evaluacion_instructor_curso(id,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,particip
 --Cambio de las secuencias para poder agregar nuevos datos
 alter sequence catalogo_cursos_id_seq restart with 785;
 alter sequence salons_id_seq restart with 59;
-alter sequence profesors_id_seq restart with 5554;
-alter sequence cursos_id_seq restart with 37;
-alter sequence participante_curso_id_seq restart with 788;
-alter sequence profesor_curso_id_seq restart with 53;
-alter sequence _evaluacion_final_curso_id_seq restart with 380;
-alter sequence _evaluacion_instructor_curso_id_seq restart with 446;
-alter sequence profesores_categorias_id_seq restart with 5529;
+alter sequence profesors_id_seq restart with 5534;
+alter sequence cursos_id_seq restart with 47;
+alter sequence participante_curso_id_seq restart with 870;
+alter sequence profesor_curso_id_seq restart with 65;
+alter sequence _evaluacion_final_curso_id_seq restart with 467;
+alter sequence _evaluacion_instructor_curso_id_seq restart with 564;
+alter sequence profesores_categorias_id_seq restart with 5535;
 
 delete from historico_tematicas where id > 0;
-copy historico_tematicas(id,nombre_curso,nombres_profesor,apellido_paterno_profesor,apellido_materno_profesor,email_profesor,telefono,tematica,semestre_anio,semestre_pi,semestre_si) from 'E:\CDD\tematicas\Tematicas.csv' delimiter '|' csv;
-alter sequence historico_tematicas_id_seq restart with 8840;
+copy historico_tematicas(id,nombre_curso,nombres_profesor,apellido_paterno_profesor,apellido_materno_profesor,email_profesor,telefono,tematica,semestre_anio,semestre_pi,semestre_si) from 'E:\CDD\migracion\Tematicas.csv' delimiter '|' csv;
+alter sequence historico_tematicas_id_seq restart with 8480;
